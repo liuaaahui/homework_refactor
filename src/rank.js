3552682d6147c705277f165407e1b43f5f5b1511
penny <925580783@qq.com>
2020-08-28 22:17:09 +0800

    [homeworkRefactor][penny]refactor: delete unused code

--- src/rank.js
+++ src/rank.js
@@ -82,26 +82,3 @@ function rating (voyage, history) {
 module.exports = {
   voyageRisk,captainHistoryRisk,voyageProfitFactor,rating
 };
-
-const voyage = {
-  zone: 'west-indies',
-  length: 10,
-};
-const history = [
-  {
-    zone: 'east-indies',
-    profit: 5,
-  },{
-    zone: 'west-indies',
-    profit: 15,
-  },{
-    zone: 'china',
-    profit: -2,
-  },
-  {
-    zone: 'west-africa',
-    profit: 7,
-  },
-];
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
