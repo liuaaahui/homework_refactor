c33d9ddcbca1f4ebad29d4b336e049e254931cd8
penny <925580783@qq.com>
2020-08-28 17:38:24 +0800

    [homeworkRefactor][penny]feature: add voyage length is 9 test

--- test/rankTest.js
+++ test/rankTest.js
@@ -14,3 +14,15 @@ rankTest('voyage length is 5', t => {
   t.is(result,expectResult);
 });
 
+rankTest('voyage length is 9', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 9,
+  };
+
+  const result = voyageRisk(voyage);
+
+  const expectResult = 9;
+
+  t.is(result,expectResult);
+});
