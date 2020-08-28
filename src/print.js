0c3408e6a67a669a99634941076729ac59df8658
penny <925580783@qq.com>
2020-08-28 21:39:13 +0800

    [homeworkRefactor][penny]refactor: extract function getDueDate

--- src/print.js
+++ src/print.js
@@ -6,16 +6,16 @@ function getOutstanding(invoice){
   return outstanding;
 }
 
+function getDueDate(){
+  const today = new Date();
+  return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
 function printOwing (invoice) {
+  invoice.dueDate = getDueDate();
   let result = '***********************\n';
   result += '**** Customer Owes ****\n';
   result += '***********************\n';
-
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-  // print details
   result += `name: ${invoice.customer}\n`;
   result += `amount: ${getOutstanding(invoice)}\n`;
   result += `amount: ${invoice.dueDate.toLocaleDateString()}\n`;
